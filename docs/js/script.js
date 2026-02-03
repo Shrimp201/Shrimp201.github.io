36e4b34be1cd0caf3ca2d97ae2ab32971ed6576a
Adam Scott <147721834+Shrimp201@users.noreply.github.com>
2026-02-03 11:19:27 +0000

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,25 +1,3 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    function show_image(src, width, height, alt) {
-        let img = document.createElement("img");
-        img.src = src;
-        img.width = width;
-        img.height = height;
-        img.alt = alt;
-
-        document.body.appendChild(img);
-    }
-
-    alert('Shrimp');
-    let shrimp = prompt("Are you a shrimp", "say yes");
-    console.log(shrimp);
-
-    if (shrimp === "yes") {
-        show_image("THESHRIMP.png", 1900, 800, "gfg logo");
-    }
-    else{
-        show_image("egg.png", 1900, 900, "gfg logo"); 
-    }
-});
 
 
 
