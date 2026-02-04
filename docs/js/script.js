c9f335e46691577e07e3ed81bf874c1fb8924b0e
Adam Scott <adam@scott.land>
2026-02-04 20:28:14 +0000

    a

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,13 +1,9 @@
-
 /* Open */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.height = "100%";
 }
 
 /* Close */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.height = "0%";
 }
-
-
-
